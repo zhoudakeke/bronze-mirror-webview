248a3c850c6802dcf518555659341ea44a7091fd
zhoudakeke <zhoudakeke@users.noreply.github.com>
2026-04-24 16:12:59 +0800

    fix: unify three.js importmap and add .gitignore

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.162.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const mirror = {
   id: 'M001',
